143f75f2eaddb29258a6b9b1abdc57d9bf92b436
Gareth Midwood <garethmidwood@Gareths-MBP.lan>
2015-11-30 23:05:05 +0000

    Add winston and fs adapters

---
+++ test/logger.js
@@ -0,0 +1,62 @@
+var expect = require('chai').expect,
+    assert = require('chai').assert,
+    sinon  = require('sinon'),
+    rewire = require('rewire'),
+    logDir = '/tmp/logger-test/';
+
+
+describe("Write logs", function() {
+    var sonumiLogger = rewire("../lib/logger");
+
+    var fsAdapterMock, logAdapterMock, sandbox;
+
+    beforeEach(function() {
+        sandbox = sinon.sandbox.create({}, {});
+
+        logAdapterMock = sinon.stub();
+        logAdapterMock.addLogFile = sinon.stub();
+
+        fsAdapterMock = sinon.stub();
+        fsAdapterMock.mkdir = sinon.stub();
+    });
+
+    afterEach(function() {
+        // restore the environment as it was before
+        sandbox.restore();
+    });
+
+
+    it('should create the log directory', function () {
+        new sonumiLogger(logDir, {
+            'fs': fsAdapterMock,
+            'logger': logAdapterMock
+        });
+
+        assert(fsAdapterMock.mkdir.calledOnce);
+    });
+
+    it('should throw an error if the log directory cannot be created', function () {
+        fsAdapterMock.mkdir.throws(new Error);
+
+        expect(function() {
+            (new sonumiLogger(logDir, {
+                'fs': fsAdapterMock,
+                'logger': logAdapterMock
+            }))
+        }).to.throw(Error);
+    });
+
+    it('shouldn\'t throw an error if the log directory already exists', function () {
+        var error = new Error();
+        error.code = 'EEXIST';
+        fsAdapterMock.mkdir.throws(error);
+
+        expect(function() {
+            (new sonumiLogger(logDir, {
+                'fs': fsAdapterMock,
+                'logger': logAdapterMock
+            }))
+        }).to.not.throw(Error);
+    });
+
+});
